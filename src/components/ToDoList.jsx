acddeb5aa08a33baeaf9d2d5796b55dd21706a19
Stiliyanss <stilianss03@gmail.com>
2025-10-28 15:15:20 +0200

    setup

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,32 @@
+import {  useState } from "react"
+
+export default function ToDoList(){
+  
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+
+    }
+
+  return(
+    <div className="to-do-list">
+    </div>
+  )
+}
